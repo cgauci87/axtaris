bff72c4e30ca4ab295f3588b61f730770157da9f
cgauci87 <cgauci@gmail.com>
2022-06-10 15:09:24 +0000

    add methods, extract data in views.py

--- axtaris-backend/stores/views.py
+++ axtaris-backend/stores/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.http import require_http_methods
-from scraping.script import aws_scrape, EbayScraper, BestBuyScraper, FlipkartScraper
+from scraping.script import aws_scrape, EbayScraper
 from stores.utiles import extract_lowest_price, success_response, error_response
 
 @require_http_methods(["GET"])
@@ -10,9 +10,7 @@ def search_products(request):
         return error_response(message="search_query is required", status_code=400)
     try:
         aws_product_list = list()
-        bestbuy_product_list = list()
         ebay_product_list = list()
-        flipkart_product_list = list()
 
         try:
             aws_product_list+=aws_scrape(search_query)
@@ -21,32 +19,16 @@ def search_products(request):
             pass
 
         try:
-            flipkart_product_list+=FlipkartScraper(search_query).scrape_products()
-        except Exception as e:
-            print("Flipkart Error:- ", e)
-            pass
-
-        try:
-            bestbuy_product_list+=BestBuyScraper(search_query).scrape_products()
-        except Exception as e:
-            print("BestBuy Error:- ", e)
-            pass
-
-
-        try:
             ebay_product_list+=EbayScraper(search_query).scrape_products()
         except:
             pass
 
-        overall_products = aws_product_list  + flipkart_product_list + bestbuy_product_list + ebay_product_list
-        # overall_products = aws_product_list  + flipkart_product_list + bestbuy_product_list
+        overall_products = aws_product_list + ebay_product_list
 
         data = {
             "final_lowest_product": extract_lowest_price(overall_products) if len(overall_products) > 0 else dict(),
             "aws_lowest_product": extract_lowest_price(aws_product_list) if len(aws_product_list) > 0 else dict(),
-            "bestbuy_lowest_product": extract_lowest_price(bestbuy_product_list) if len(bestbuy_product_list) > 0 else dict(),
             "ebay_lowest_product": extract_lowest_price(ebay_product_list) if len(ebay_product_list) > 0 else dict(),
-            "flipkart_lowest_product": extract_lowest_price(flipkart_product_list) if len(flipkart_product_list) > 0 else dict(),
             "total_products": overall_products,
         }
         return success_response(data=data, message="Success")
