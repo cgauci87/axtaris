92f0890b2caade10aace91dcd831e0b769a7ee0d
cgauci87 <cgauci@gmail.com>
2022-06-14 08:18:07 +0000

    add comments on functions related to scraper in script.py

--- axtaris-backend/scraping/script.py
+++ axtaris-backend/scraping/script.py
@@ -8,44 +8,38 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Create an Extractor by reading from the YAML file
 e = Extractor.from_yaml_file(f'{BASE_DIR}/scraping/search_results.yml')
 
-def aws_scrape(product_name):
-    aws_product = list()
-    url = f"https://www.amazon.com/s?k={product_name}"
-    headers = {
-        'dnt': '1',
-        'upgrade-insecure-requests': '1',
-        'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_4) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.61 Safari/537.36',
-        'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
-        'sec-fetch-site': 'same-origin',
-        'sec-fetch-mode': 'navigate',
-        'sec-fetch-user': '?1',
-        'sec-fetch-dest': 'document',
-        'referer': 'https://www.amazon.com/',
-        'accept-language': 'en-GB,en-US;q=0.9,en;q=0.8',
-    }
-    # Download the page using requests
-    r = requests.get(url, headers=headers)
-    # Simple check to check if page was blocked (Usually 503)
-    if r.status_code > 500:
-        if "To discuss automated access to Amazon data please contact" in r.text:
-            print("Page %s was blocked by Amazon. Please try using better proxies\n"%url)
-        else:
-            print("Page %s must have been blocked by Amazon as the status code was %d"%(url,r.status_code))
-        return None
-    # Pass the HTML of the page and create
-    for product in e.extract(r.text)['products']:
-        if product['price'] == None or product['price'] == "None":
-            continue
-        aws_product.append(
-            {
-                "title": product['title'],
-                "price": float(product['price'].replace("$", "").replace(",", "")),
-                "url": f"https://www.amazon.com{product['url']}",
-            }
-        )
-    return aws_product
+# Flipkart Scaper - To obtain data from the source itself
+class FlipkartScraper:
 
+    def __init__(self, keyword):
+        self.keyword = keyword
+        plusified_keyword = keyword.replace(" ", "+")
+        self.products = []
+        self.search_url = "https://www.flipkart.com/search?q=" + plusified_keyword
 
+    def scrape_products(self):
+        headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.132 Safari/537.36'}
+        content = requests.get(self.search_url, headers=headers).text
+        soup = BeautifulSoup(content, "html.parser")
+        product_list = []
+        products = soup.find("div", {"class": "_1YokD2 _3Mn1Gg"}).find_all("div", {"class": "_1AtVbE col-12-12"})
+        for product in products:
+            try:
+                div = product.find("div", {"class": "_13oc-S"})
+                url = f'https://www.flipkart.com{div.div.div.a["href"]}'
+                name = div.find('div', {'class': '_4rR01T'}).text
+                price = div.find('div', {"class": "_30jeq3 _1_WHN1"}).text
+
+                product_list.append({
+                    "title": name,
+                    "price": round(float(price.replace("₹", "").replace(",", ""))/77.55, 2),
+                    "url": url
+                })
+            except:
+                pass
+        return product_list
+
+# Ebay Scaper - To obtain data from the source itself
 class EbayScraper:
 
     def __init__(self, keyword):
--- axtaris-backend/stores/views.py
+++ axtaris-backend/stores/views.py
@@ -1,5 +1,5 @@
 from django.views.decorators.http import require_http_methods
-from scraping.script import aws_scrape, EbayScraper
+from scraping.script import FlipkartScraper, EbayScraper
 from stores.utiles import extract_lowest_price, success_response, error_response
 
 @require_http_methods(["GET"])
@@ -9,25 +9,24 @@ def search_products(request):
     if search_query is None:
         return error_response(message="search_query is required", status_code=400)
     try:
-        aws_product_list = list()
+        flipkart_product_list = list()
         ebay_product_list = list()
 
         try:
-            aws_product_list+=aws_scrape(search_query)
+            flipkart_product_list+=FlipkartScraper(search_query).scrape_products()
         except Exception as e:
-            print("AWS Error:- ", e)
+            print("Flipkart Error:- ", e)
             pass
-
         try:
             ebay_product_list+=EbayScraper(search_query).scrape_products()
         except:
             pass
 
-        overall_products = aws_product_list + ebay_product_list
+        overall_products = flipkart_product_list + ebay_product_list
 
         data = {
             "final_lowest_product": extract_lowest_price(overall_products) if len(overall_products) > 0 else dict(),
-            "aws_lowest_product": extract_lowest_price(aws_product_list) if len(aws_product_list) > 0 else dict(),
+            "flipkart_lowest_product": extract_lowest_price(flipkart_product_list) if len(flipkart_product_list) > 0 else dict(),
             "ebay_lowest_product": extract_lowest_price(ebay_product_list) if len(ebay_product_list) > 0 else dict(),
             "total_products": overall_products,
         }
